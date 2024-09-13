a632c7704a168820bb5403bbf3584eb2f62d376c
Ahmed Hany <ah7608867@gmail.com>
2024-09-13 19:29:06 +0300

    fetch recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,50 @@
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+console.log('Here we goo');
+
+// 1) Load recipe data
+/*
+API link :https://forkify-api.herokuapp.com/v2
+Link of Get recipe/Delete recipe: https://forkify-api.herokuapp.com/api/v2/recipes/ID
+
+*/
+const LoadRecipe = async function () {
+  try {
+    // Fetching data from API
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    // Check if the response is ok, if not throw an error
+    if (response.status !== 200) {
+      throw new Error(
+        `Couldn't fetch the recipe data, API status: ${response.status}`
+      );
+    }
+
+    // Convert the response to JSON to get the recipe data
+    const data = await response.json();
+    if (data.status !== 'success') {
+      throw new Error(`No data found, ${data.message}`);
+    }
+
+    // Destructure the data object to get the recipe data
+    let { recipe } = data.data;
+    recipe = {
+      title: recipe.title,
+      id: recipe.id,
+      publisher: recipe.publisher,
+      img_url: recipe.image_url,
+      source_url: recipe.source_url,
+      servings: recipe.servings,
+      cooking_time: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    // Catch the error and log it
+    alert(err.message);
+    //console.error(err.message);
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+LoadRecipe();
